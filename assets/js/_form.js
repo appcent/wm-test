99046a32b64c97015429e2a85c048a4d4b71e1ca
Lion <helldrumer@gmail.com>
2018-12-08 23:07:12 +0300

    range

--- assets/js/_form.js
+++ assets/js/_form.js
@@ -1,5 +1,7 @@
 import $ from 'jquery';
 import Inputmask from "inputmask/dist/inputmask/inputmask.numeric.extensions";
+import noUiSlider from 'nouislider';
+import wNumb from 'wnumb';
 
 $(document)
     .ready(() => {
@@ -13,6 +15,23 @@ $(document)
 
         const im = new Inputmask('+7(999)999-99-99');
         im.mask($('[type="tel"]')[0]);
+
+        const $range = $('.b-range__slider')[0];
+        const $rangeRes = $('.b-range__input input');
+		noUiSlider.create($range, {
+			start: [20, 60],
+			connect: [false, true, false],
+            step: 1,
+			range: {
+				min: 0,
+				max: 100
+			},
+			cssPrefix: 'b-range-',
+			format: wNumb({decimals: false})
+		});
+		$range.noUiSlider.on('update', (values, handle) => {
+			$rangeRes[handle].value = values[handle];
+        });
 
     })
     .on('click', '.b-input-number__btn', e => {
@@ -30,6 +49,11 @@ $(document)
     .on('input', '.js-field', e => {
         const $field = $(e.target).closest('.js-field');
         isActive($field);
+    })
+    .on('change keyup', '.b-range__input input', e => {
+		const $target = $(e.target).closest('.b-range__input input');
+		console.log($target);
+		$('.b-range__slider')[0].noUiSlider.setHandle($target.index(), $target.val());
     });
 
 const isActive = $field => {
