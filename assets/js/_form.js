af615a0bc2a7be6a53b97eb60971860cf68c63db
Lion <helldrumer@gmail.com>
2018-12-09 01:42:23 +0300

    range end

--- assets/js/_form.js
+++ assets/js/_form.js
@@ -5,37 +5,44 @@ import wNumb from 'wnumb';
 
 $(document)
     .ready(() => {
-
+		/* Active */
         const $field = $('.js-field');
         if ($field.length) {
             $field.each(function () {
                 isActive($(this));
             });
         }
-
-        const im = new Inputmask('+7(999)999-99-99');
-        im.mask($('[type="tel"]')[0]);
-
+		/* Mask */
+		const $phone = $('[type="tel"]');
+		if ($phone.length) {
+			const im = new Inputmask('+7(999)999-99-99');
+			$phone.each((index, elem) => im.mask(elem));
+		}
+		/* Range */
         const $range = $('.b-range__slider');
         if ($range.length) {
-			noUiSlider.create($range[0], {
-				start: [40, 60],
-				connect: [false, true, false],
-				step: 1,
-				range: {
-					min: 0,
-					max: 100
-				},
-				cssPrefix: 'b-range-',
-				format: wNumb({decimals: false})
-			}).on('update', (values, handle) => {
-				const $input = $range.siblings('.b-range__result').find('.js-range-field');
-				$input[handle].value = values[handle];
-				$input[handle].setAttribute('data-range', handle);
+			$range.each(function (index, elem) {
+				let $this = $(this);
+				noUiSlider.create(elem, {
+					start: [40, 60],
+					connect: [false, true, false],
+					step: 1,
+					range: {
+						min: 0,
+						max: 100
+					},
+					cssPrefix: 'b-range-',
+					format: wNumb({decimals: false})
+				}).on('update', (values, handle) => {
+					const $input = $this.siblings('.b-range__result').find('.js-range-field');
+					$input[handle].value = values[handle];
+					$input[handle].setAttribute('data-range', handle);
+				});
 			});
 		}
 
     })
+	/* Number */
     .on('click', '.b-input-number__btn', e => {
        e.preventDefault();
        const $btn = $(e.target).closest('.b-input-number__btn'),
@@ -48,16 +55,18 @@ $(document)
        }
 		isActive($input);
     })
+	/* Active */
     .on('input', '.js-field', e => {
         const $field = $(e.target).closest('.js-field');
         isActive($field);
     })
+	/* Range */
     .on('change keyup', '.js-range-field', e => {
 		const $target = $(e.target).closest('.js-range-field');
 		const slider = $target.parents('.b-range__result').siblings('.b-range__slider')[0];
 		slider.noUiSlider.setHandle($target.attr('data-range'), $target.val());
     });
-
+/* Active */
 const isActive = $field => {
     const $input = $field.parent('.b-form__elem');
     $field.val().length && !$field.hasClass('is-active') ? $input.addClass('is-active') : $input.removeClass('is-active');
